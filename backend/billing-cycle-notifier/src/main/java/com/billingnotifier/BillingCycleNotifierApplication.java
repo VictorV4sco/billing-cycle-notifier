699bf728cdf7a337b5445c03c7d8688b5c948670
Victor Da Costa Vasco <vc.vasco1234@hotmail.com>
2026-06-11 11:22:51 -0300

    chore: initialize backend and mobile apps

---
+++ backend/billing-cycle-notifier/src/main/java/com/billingnotifier/BillingCycleNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.billingnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingCycleNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingCycleNotifierApplication.class, args);
+	}
+
+}
---
+++ backend/billing-cycle-notifier/src/test/java/com/billingnotifier/BillingCycleNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billingnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingCycleNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
